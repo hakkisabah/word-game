0419c07d79e3dd00ebbaf55e655f9f4905aaa449
Hakki SABAH <hakkisabah@hotmail.com.tr>
2021-03-19 01:05:31 +0300

    all last committed codes rearranged to develop

--- __tests__/wordgameapp.js
+++ __tests__/wordgameapp.js
@@ -1,22 +1,40 @@
 import WordGameApp from '../src/app.js';
 import names from '../src/data/names.json';
 
+const mockingForChainCorrectAnswer = (char) => {
+  return names.find((name) => name[0] === char);
+};
+const mockingForChainWrongAnswer = (char) => {
+  return names.find((name) => name[0] !== char);
+};
+
 describe('Word Game Instance', () => {
   const WordGame = new WordGameApp(names);
   let shuffledName = '';
-  it('Get names array from Instance variable', () => {
+  it('get names array from Instance variable', () => {
     expect(Array.isArray(WordGame.names)).toBe(true);
   });
-  it('Load Instance', () => {
+
+  it('get word after shuffled', () => {
     shuffledName = WordGame.getNameAfterShuffle();
     expect(shuffledName).toMatch(/^[A-Za-zıİşŞöÖüÜçÇğ]+$/);
   });
-  it('Answer loaded name correctly', () => {
-    const answered = WordGame.answerLoadedName(shuffledName);
-    expect(answered).toBe(true);
+
+  it('answer loaded name correctly', () => {
+    const answerStartedWithInputLastChar =
+      shuffledName[shuffledName.length - 1];
+    expect(answerStartedWithInputLastChar).toEqual(
+      mockingForChainCorrectAnswer(answerStartedWithInputLastChar)[0]
+    );
   });
-  it('Answer loaded name wrongly', () => {
-    const answered = WordGame.answerLoadedName('wrong answer');
-    expect(answered).toBe(false);
+
+  it('answer name wrongly', () => {
+    const answered = mockingForChainWrongAnswer(shuffledName);
+    expect(WordGame.answerLoadedName(answered)).toBe(false);
+  });
+
+  it('answered name', () => {
+    WordGame.findedNames[shuffledName] = 1;
+    expect(typeof WordGame.answerLoadedName(shuffledName)).toEqual('string');
   });
 });
--- src/app.js
+++ src/app.js
@@ -2,14 +2,47 @@ export default class WordGameApp {
   constructor(names) {
     this.names = names;
     this.currentName = '';
+    this.findedNames = [];
   }
   getNameAfterShuffle() {
     const totalNames = this.names.length;
     const selectedNameOrderNumber = Math.floor(Math.random() * totalNames);
     this.currentName = this.names[selectedNameOrderNumber];
+    if (this.findedNames[this.currentName]) {
+      this.getNameAfterShuffle();
+    }
     return this.currentName;
   }
   answerLoadedName(answer) {
-    return answer === this.currentName ? true : false;
+    const isAnswerOk = this.checkAnswer(answer);
+    // if isAnswerOk is string, we know this because answered before
+    if (typeof isAnswerOk === 'string') {
+      return isAnswerOk;
+    }
+    return isAnswerOk ? true : false;
+  }
+  checkAnswer(answer) {
+    if (!this.findedNames[answer]) {
+      const findedAnswer = this._findAnswer(answer);
+      const findedAnswerCheckLastCharEquality = findedAnswer
+        ? this._checkLastChar(findedAnswer)
+        : false;
+      if (findedAnswerCheckLastCharEquality) {
+        this.findedNames[findedAnswer] = 1;
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return answer;
+    }
+  }
+  _findAnswer(answer) {
+    return this.names.find((name) => {
+      return name === answer;
+    });
+  }
+  _checkLastChar(findedAnswer) {
+    return this.currentName[this.currentName.length - 1] === findedAnswer[0];
   }
 }
