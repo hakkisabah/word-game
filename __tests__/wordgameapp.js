6ab0a77f764132696057d9b3d95d119bd4f719cd
Hakki SABAH <hakkisabah@hotmail.com.tr>
2021-03-18 18:14:46 +0300

    Test names array, answer correct and wrong cases

--- __tests__/wordgameapp.js
+++ __tests__/wordgameapp.js
@@ -2,9 +2,21 @@ import WordGameApp from '../src/app.js';
 import names from '../src/data/names.json';
 
 describe('Word Game Instance', () => {
+  const WordGame = new WordGameApp(names);
+  let shuffledName = '';
+  it('Get names array from Instance variable', () => {
+    expect(Array.isArray(WordGame.names)).toBe(true);
+  });
   it('Load Instance', () => {
-    const WordGame = new WordGameApp(names);
-    const shuffledNames = WordGame.getNameAfterShuffle();
-    expect(shuffledNames).toMatch(/^[A-Za-zıİşŞöÖüÜçÇğ]+$/);
+    shuffledName = WordGame.getNameAfterShuffle();
+    expect(shuffledName).toMatch(/^[A-Za-zıİşŞöÖüÜçÇğ]+$/);
+  });
+  it('Answer loaded name correctly', () => {
+    const answered = WordGame.answerLoadedName(shuffledName);
+    expect(answered).toBe(true);
+  });
+  it('Answer loaded name wrongly', () => {
+    const answered = WordGame.answerLoadedName('wrong answer');
+    expect(answered).toBe(false);
   });
 });
--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 export default class WordGameApp {
   constructor(names) {
     this.names = names;
+    this.currentName = '';
   }
   getNameAfterShuffle() {
     const totalNames = this.names.length;
     const selectedNameOrderNumber = Math.floor(Math.random() * totalNames);
-    return this.names[selectedNameOrderNumber];
+    this.currentName = this.names[selectedNameOrderNumber];
+    return this.currentName;
+  }
+  answerLoadedName(answer) {
+    return answer === this.currentName ? true : false;
   }
 }
