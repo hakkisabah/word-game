8261e4950e700f35a05b757d6d99b08188839685
Hakki SABAH <hakkisabah@hotmail.com.tr>
2021-03-31 12:52:26 +0300

    and all that remains

--- README.md
+++ README.md
@@ -1,9 +1,28 @@
 # word-game
+#### Demo : https://hakkisabah.github.io/word-game/dist/index.html 
 
-## Expect
-The word game plans to show some affirmative behavior to the user who can derive new names from the last letter of the nouns.
+## Explanation
+
+The word game uses the user's microphone through the user's browser, the game is played by deriving a new name from the last letter of the word displayed on the screen.
+
+The rules that apply to each game are:
+- Name repetition cannot be made within the same ongoing game.
+- Each new name found must begin with the last letter of the previous name.
+- These operations should be done within 8 seconds.
+
+Requirements for play:
+- Working microphone
+- Latest version Chrome Browser
+- A quiet environment
 
 ### Development
 Configured all required development environment.
 
 You should be typing to your terminal `npm install` while in word-game folder after the cloning this repository for developing and understanding
+
+### Diagram
+Information about how the code distribution takes place can be obtained from the diagram.
+
+As seen in this diagram, the helpers can communicate with each other only through the application trigger(index.js).
+
+![SetupIndex](src.png)
--- __tests__/wordgameapp.js
+++ __tests__/wordgameapp.js
@@ -1,23 +1,57 @@
 import WordGameApp from '../src/app.js';
 import names from '../src/data/names.json';
-
+import enNames from '../src/data/en-names.json';
+import { appCenter, appHelper } from '../src/helpers/importreturner';
 const mockingForChainCorrectAnswer = (char) => {
   return names.find((name) => name[0] === char);
 };
 const mockingForChainWrongAnswer = (char) => {
   return names.find((name) => name[0] !== char);
 };
-
+const GameProps = {
+  appCenter,
+  appHelper,
+};
+const props = {
+  answer: null,
+  findedNames: [],
+  names: names,
+  currentName: '',
+};
+const confiugredData = {
+  language: {
+    detectedLang: 'en',
+  },
+  game: {},
+};
 describe('Word Game Instance', () => {
-  const WordGame = new WordGameApp(names);
+  const WordGame = new WordGameApp({ confiugredData, GameProps });
   let shuffledName = '';
   it('get names array from Instance variable', () => {
-    expect(Array.isArray(WordGame.names)).toBe(true);
+    props.names = names;
+    expect(Array.isArray(props.names)).toBe(true);
   });
 
-  it('get word after shuffled', () => {
-    shuffledName = WordGame.getNameAfterShuffle();
+  it('checking each name characters for Turkish', () => {
+    for (let i = 0; i < names.length; i++) {
+      expect(names[i]).toMatch(/^[A-Za-zıİşŞöÖüÜçÇğ]+$/);
+    }
+  });
+
+  it('checking each name characters for English', () => {
+    for (let i = 0; i < enNames.length; i++) {
+      expect(enNames[i]).toMatch(/^[A-Za-z]+$/);
+    }
+  });
+
+  it('get word after shuffled for Turkish', () => {
+    shuffledName = WordGame.getNameAfterShuffle(names);
     expect(shuffledName).toMatch(/^[A-Za-zıİşŞöÖüÜçÇğ]+$/);
+  });
+
+  it('get word after shuffled for English', () => {
+    shuffledName = WordGame.getNameAfterShuffle(enNames);
+    expect(shuffledName).toMatch(/^[A-Za-z]+$/);
   });
 
   it('answer loaded name correctly', () => {
@@ -29,12 +63,18 @@ describe('Word Game Instance', () => {
   });
 
   it('answer name wrongly', () => {
-    const answered = mockingForChainWrongAnswer(shuffledName);
-    expect(WordGame.answerLoadedName(answered)).toBe(false);
+    props.answer = mockingForChainWrongAnswer(shuffledName[0]);
+    expect(GameProps.appCenter.gameLogic({ props }).answerLoadedName()).toBe(
+      false
+    );
   });
 
   it('answered name', () => {
-    WordGame.findedNames[shuffledName] = 1;
-    expect(typeof WordGame.answerLoadedName(shuffledName)).toEqual('string');
+    WordGame.findedNames.push({ [WordGame.currentName]: 1 });
+    props.answer = shuffledName;
+    props.findedNames = WordGame.findedNames;
+    expect(
+      typeof GameProps.appCenter.gameLogic({ props }).answerLoadedName()
+    ).toEqual('string');
   });
 });
